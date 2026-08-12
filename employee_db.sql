8d337e7c227ebb524ec81a09a3638ed4e6ab82fc
ravali <rudrapuravali@gmail.com>
2026-08-12 17:48:03 +0530

    employee

--- employee_db.sql
+++ employee_db.sql
@@ -44,6 +44,74 @@ select * from employees;
 update employees
 set
 net_salary=gross_salary-pf-tax;
+select * from employees;
+alter table employees
+add constraint chk_salary_positive check (salary>0);
+select * from employees;
+select employee_id,
+name,
+salary,
+case
+when salary>0 then 'vaild'
+when salary<=0 then 'invaild:negativeor zero'
+when salary is null then 'invaild:null'
+end as salary_validation
+from employees;
+insert into employees(employee_id, name, department, designation, salary)
+values
+(4, 'sai', 'IT', 'Developer', 50000),
+(5, 'goutham', 'IT', 'Developer', 45000),
+(6, 'bunny', 'IT', 'developer', 50000);
+select * from employees;
+select employee_id,name,salary
+from employees
+where employee_id<=0 or employee_id is null;
+alter table employees
+add constraint chk_employee_id_positive check(employee_id>0);
+select * from employees;
+select
+employee_id,
+name,
+case
+when employee_id>0 then 'vaild'
+when employee_id<=0 then 'invaild:zero or negative'
+when employee_id is null then 'invaild:null'
+end as employee_id_vaildation
+from employees;
+select
+employee_id,
+name,
+case
+when name is null then 'invaild:null'
+when name=''then 'invaild:empty'
+when name not regexp '^[a-zA-Z ]+$' then 'invaild:contain numbers/symbols'
+else 'vaild'
+end as name_vaildation
+from employees;
+select
+case
+when salary>=50000 then 'A'
+when salary>=30000 then 'B'
+when salary>=15000 then 'c'
+else 'D'
+end as salary_grade,
+count(*) as total_employee
+from employees
+group by salary_grade
+order by salary_grade;
+select*from employees;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
